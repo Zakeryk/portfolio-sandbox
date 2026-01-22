e0d4440f4a17987a7e9754fdec336e59939dc342
Zakeryk <zakeryk@gmail.com>
2026-01-21 18:42:43 -0700

    Deploy: 2026-01-21 18:42

--- FINCRAFT/src/game/GameEngine.js
+++ FINCRAFT/src/game/GameEngine.js
@@ -735,8 +735,8 @@ export class GameEngine {
       const texture = await PIXI.Assets.load(spriteConfig.path)
       const sprite = new PIXI.Sprite(texture)
       sprite.anchor.set(spriteConfig.anchorX, spriteConfig.anchorY)
-      sprite.width = (spriteConfig.displayWidth || spriteConfig.width) * 1.4
-      sprite.height = (spriteConfig.displayHeight || spriteConfig.height) * 1.4
+      sprite.width = (spriteConfig.displayWidth || spriteConfig.width) * 1.33
+      sprite.height = (spriteConfig.displayHeight || spriteConfig.height) * 1.33
       sprite.y = 22  // offset to align with tile
       container.addChild(sprite)
     } catch (e) {
